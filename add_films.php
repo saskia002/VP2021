f897bf4c08269fca48c4567b0fe6aeb405d71860
Siim002 <siikri@greeny.cs.tlu.ee>
2021-09-26 20:29:27 +0300

    4. kodutöö

--- add_films.php
+++ add_films.php
@@ -1,23 +1,52 @@
 <?php
 	$author_name = "Siim";
+	//palceholderid
+	$title_form_placeholder = 'Filmi pealkiri';
+	$year_form_placeholder = '';
+	$duration_form_placeholder = '60';
+	$genre_form_placeholder = 'Žanr';
+	$studio_form_placeholder = 'Filmi tootja';
+	$director_form_placeholder = 'Režissöör';
+	
 	require_once("../../config.php");
 	//echo $server_host;
 	require_once("fnc_film.php");
 
-	$film_store_notice = null;
+
+	$film_store_notice = '';
 
 	//kas pyytakse salvestata
 	if(isset($_POST['film_submit'])){
-		//kontrollin et on ikka sisestaud
-		if((!empty($_POST['title_input'])) and (!empty($_POST['year_input'])) and (!empty($_POST['genre_input'])) and (!empty($_POST['studio_input'])) and (!empty($_POST['director_input']))){
-			
-			$film_store_notice = store_film($_POST['title_input'], $_POST['year_input'], $_POST['duration_input'], $_POST['genre_input'], $_POST['studio_input'], $_POST['director_input']);
-			
+		if((!empty($_POST['title_input']))){
+			$title_input = filter_var($_POST['title_input'], FILTER_SANITIZE_STRING);
+			$title_form_placeholder = $_POST['title_input'];
+		}
+		if((!empty($_POST['year_input']))){
+			$year_input = strval(filter_var($_POST['year_input'], FILTER_SANITIZE_NUMBER_INT));
+			$year_form_placeholder = $_POST['year_input'];
+		}
+		if((!empty($_POST['duration_input']))){
+			$duration_input = strval(filter_var($_POST['duration_input'], FILTER_SANITIZE_NUMBER_INT));
+			$duration_form_placeholder = $_POST['duration_input'];
+		}
+		if((!empty($_POST['genre_input']))){
+			$genre_input = filter_var($_POST['genre_input'], FILTER_SANITIZE_STRING);
+			$genre_form_placeholder = $_POST['genre_input'];
+		}
+		if(!empty($_POST['studio_input'])){
+			$studio_input = filter_var($_POST['studio_input'], FILTER_SANITIZE_STRING);
+			$studio_form_placeholder = $_POST['studio_input'];
+		}
+		if((!empty($_POST['director_input']))){
+			$director_input = filter_var($_POST['director_input'], FILTER_SANITIZE_STRING);
+			$director_form_placeholder = $_POST['director_input'];
 		}else{
 			$film_store_notice = "Osa andmeid on puudu!";
 		}
+			if((!empty($_POST['title_input'])) and (!empty($_POST['year_input'])) and (!empty($_POST['duration_input'])) and (!empty($_POST['genre_input'])) and (!empty($_POST['studio_input'])) and (!empty($_POST['director_input']))){
+			$film_store_notice = store_film($title_input, $year_input, $duration_input, $genre_input, $studio_input, $director_input);
+			}
 	}
-
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -37,24 +66,24 @@
 	<h2>Eesti filmide lisamine andmebaasi:</h2>
 	
 	<!-- andmebaasi saatmine, id= on php jaoks ja name htmli jaoks -->
-	<form method="POST">
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" >
 		<label for="title_input">Filmi pealkiri</label>
-		<input type="text" name="title_input" id="title_input" placeholder="Filmi pealkiri">
+		<input type="text" name="title_input" id="title_input" placeholder="<?php echo $title_form_placeholder; ?>" required>
 		<br />
 		<label for="year_input">Valmimisaasta</label>
-		<input type="number" name="year_input" id="year_input" min="1912">
+		<input type="number" name="year_input" id="year_input" min="1912"  >
 		<br />
 		<label for="duration_input">Kestus</label>
-		<input type="number" name="duration_input" id="duration_input" min="1" value="60" max="600">
+		<input type="number" name="duration_input" id="duration_input" min="1" placeholder="<?php echo $duration_form_placeholder; ?>" max="600" required>
 		<br />
 		<label for="genre_input">Žanr</label>
-		<input type="text" name="genre_input" id="genre_input" placeholder="Žanr">
+		<input type="text" name="genre_input" id="genre_input" placeholder="<?php echo $genre_form_placeholder; ?>" required>
 		<br />
 		<label for="studio_input">Filmi tootja</label>
-		<input type="text" name="studio_input" id="studio_input" placeholder="Filmi tootja">
+		<input type="text" name="studio_input" id="studio_input" placeholder="<?php echo $studio_form_placeholder; ?>" required>
 		<br />
 		<label for="director_input">Režissöör</label>
-		<input type="text" name="director_input" id="director_input" placeholder="Režissöör">
+		<input type="text" name="director_input" id="director_input" placeholder="<?php echo $director_form_placeholder; ?>" required> 
 		<br />
 		<input type="submit" name="film_submit" value="Salvesta">
 	</form>
