989c9411e9bba68998c1b6f7ce2ade2f188d560f
siim002 <siikri@greeny.cs.tlu.ee>
2021-11-07 15:54:49 +0200

    asdasdas

--- page_login.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	<input type="email" name="email_input" placeholder="email" value="<?php echo htmlspecialchars($inserted_username); ?>">
-	<input type="password" name="password_input" placeholder="salasõna">
-	<input type="submit" name="login_submit" value="Logi sisse"><?php echo "\t"; echo $login_error; ?>
-</form>
